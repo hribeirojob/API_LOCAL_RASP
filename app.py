634d0a9e4f61ba88c91ee1bffe8f3a03eef775d1
Hugo <hribeiro.job@gmail.com>
2018-05-22 11:41:22 -0300

    adicionando as modificaçoes das rotas para padronizar com o front

--- app.py
+++ app.py
@@ -23,33 +23,36 @@ def index():
     return render_template('index.html', async_mode=socketio.async_mode)
 
 #Leitor do QR Code
-@socketio.on('QRCode7980g', namespace='/test')
+@socketio.on('QRCode7980g')
 def read_qr():
+   print('Requisitou QRCode 7980g')
    try:
        Obj_porta = serial.Serial('/dev/ttyACM0', 9600)
-       valor =  Obj_porta.readline(6)
+       qrcode =  Obj_porta.readline(6)
    except serial.SerialException:
-       valor = 'ERRO'
-   emit('resposta', {'data': [valor]})
-   print (valor)
+       qrcode = 'ERRO QRCode'
+   emit('QRCode7980g', {'data': qrcode})
+   print (qrcode)
 
 #Leitor da balanca
-@socketio.on('balanca_2098', namespace='/test')
+@socketio.on('balanca_2098')
 def getBalanca_2098():
+   print('reqisitou Peso Balanca Toledo_2098')
    while 1:
       try:
           Port_bal = serial.Serial('/dev/ttyUSB0',9600)
           bal = Port_bal.readline(7)
           time.sleep(0.25)
       except serial.SerialException:
-          bal = 'ERRO'
-      emit('resposta',{'data': [bal]})
+          bal = 'ERRO Balanca nao conectada'
+      emit('balanca_2098',{'data': bal})
       print (bal)
 
 #Leitor do cartao de Usuario
-@socketio.on('cardUser', namespace='/test')
+@socketio.on('cardUser')
 def Get_Card_User():
-
+   
+   print('Requisitou Cartao Usuario')
    continue_reading = True
 
    while continue_reading:
@@ -68,35 +71,36 @@ def Get_Card_User():
       # Se conseguir o  UID do cartao, continue
        if status == MIFAREReader.MI_OK:
           id_card =  str(uid[0])+"."+str(uid[1])+"."+str(uid[2])+"."+str(uid[3])
-	  emit('resposta',{'data': [id_card]})
+	  emit('cardUser',{'data': id_card})
 	  print (id_card)
           continue_reading = False
           GPIO.cleanup()
 
     
 
-@socketio.on('disconnect_request', namespace='/test')
+@socketio.on('disconnect_request')
 def disconnect_request():
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('resposta',
-         {'data': 'Voce desconectou!'})
-    print ('Cliente desconectou!')
+    print('Cliente Pediu Para Desconectar')
+    emit('disconect',
+         {'data': 'Voce foi Desconectado!'})
+    print ('Cliente foi Desconectado!')
     disconnect()
 
 
 #Conexao com o Cliente
-@socketio.on('connect', namespace='/test')
+@socketio.on('connect')
 def test_connect():
-    emit('my_response', {'data': 'Connected', 'count': 0})
+    print ('Cliente Pediu para Conectar')
+    emit('connect', {'data': 'Connected'})
     print ('Cliente conectado',request.sid)
 
 @socketio.on('disconnect', namespace='/test')
 def test_disconnect():
-    emit('resposta',
+    emit('disconect',
          {'data': 'Voce foi desconectado!'})
 
     print('Client disconnected', request.sid)
 
 
 if __name__ == '__main__':
-    socketio.run(app,host='0.0.0.0', debug=True)
+    socketio.run(app, host = '0.0.0.0', port = '3000', debug=True)
